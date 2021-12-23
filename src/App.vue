e152faeba2f72fe8bc5fced36b95e27627530b35
Orion <ahmed.meklad.news@gmail.com>
2021-12-23 17:03:53 +0200

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Promoters.com</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      token: "1|sX8SjZxrk0Pnj0cZMJvtMhbELPptIDf5bJL5XapW",
+      name: "brand three"
+    }
+  },
+  mounted () {
+    let data = new FormData();
+    data.append("name", this.name)
+    // this.axios.get("http://promoters.owl/api/brands" , { headers: {"Authorization" : `Bearer ${this.token}`} })
+    //           .then(response => (console.log(response)));
+    this.axios.post("http://promoters.owl/api/brands/store" , data
+              , { headers: {"Authorization" : `Bearer ${this.token}`} })
+              .then(response => (console.log(response)));
+
+    // this.axios.post("http://promoters.owl/api/login" , { 
+    //   "email": "Client@gmail.com",
+    //   "password": "01200902377"
+    //  }).then(response => (console.log(response)));
   }
 }
 </script>
